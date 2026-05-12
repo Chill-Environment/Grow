b097055f4c03635198890c12d977df9649bbc92a
LGC <lucaspereiraovalle@gmail.com>
2026-05-12 20:45:35 -0300

    Delete src directory

--- src/telegram.h
+++
@@ -1,8 +0,0 @@
-#ifndef TELEGRAM_H
-#define TELEGRAM_H
-
-void initTelegram();
-void handleTelegramMessages();
-void enviarTelegram(const String& mensaje);
-
-#endif
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void iniciarModoAP();
-bool conectarWiFi();
-void setupConfigServer();  // solo para modo AP
-
-#endif
